87276966da1629de52c689d4cf88767ca6e87c8c
wwy <wangweiyi@linzikg.com>
2025-10-12 16:02:14 +0800

    feat: 对阵列表

--- src/views/champion/Against.vue
+++ src/views/champion/Against.vue
@@ -1,73 +1,139 @@
 <script setup>
 import Layout from "@/components/Layout.vue";
-import {computed, onMounted, ref} from "vue";
-import {getMatchStageApi} from "@/api/champion";
-import {requireImg, isNotEmptyObj} from "@/utils/common";
-import {ElMessage} from "element-plus";
-import luckyStar from '@/assets/openBox/luckyStar.png'
-import Detail from './Detail.vue';
+import { computed, onMounted, ref } from "vue";
+import { getMatchStageApi } from "@/api/champion";
+import { requireImg, isNotEmptyObj } from "@/utils/common";
+import { ElMessage } from "element-plus";
+import luckyStar from "@/assets/openBox/luckyStar.png";
+import Detail from "./Detail.vue";
 import { useRouter } from "vue-router";
 
-const router = useRouter()
-const loading = ref(false)
-const levelData = ref([])
+const router = useRouter();
+const loading = ref(false);
+const levelData = ref([]);
 const statusMap = {
-  '0': '未开始',
-  '1': '进行中',
-  '2': '已结束'
-}
+  0: "未开始",
+  1: "进行中",
+  2: "已结束",
+};
 const statusColor = computed(() => {
   return (status) => {
     switch (status) {
-      case 0: return '#FF952A';
-      case 1: return '#FF3C2A';
-      case 2: return '#602bCF';
-      default: return '#AAAAAA';
+      case 0:
+        return "#FF952A";
+      case 1:
+        return "#FF3C2A";
+      case 2:
+        return "#602bCF";
+      default:
+        return "#AAAAAA";
     }
-  }
-})
+  };
+});
 
 // 获取比赛阶段
 const getMatchStageList = () => {
-  loading.value = true
-  const matchId = Number(window.sessionStorage.getItem('matchId'))
+  loading.value = true;
+  const matchId = Number(window.sessionStorage.getItem("matchId"));
   getMatchStageApi({
-    matchId: matchId
-  }).then(res => {
-    if (res.data && res.data.length) {
-      levelData.value = res.data
-    }
-  }).finally(() => {
-    loading.value = false
+    matchId: matchId,
   })
-}
+    .then((res) => {
+      if (res.data && res.data.length) {
+        levelData.value = res.data;
+      }
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
 
 const handleJump = (type) => {
-  window.sessionStorage.setItem('againstType', type)
-  router.push('/match-against-detail')
-}
+  window.sessionStorage.setItem("againstType", type);
+  router.push("/match-against-detail");
+};
 
 onMounted(() => {
-  getMatchStageList()
-})
+  getMatchStageList();
+});
+import roundBg from "@/assets/images/match/round-bg.png";
+import bg4 from "@/assets/images/match/4bg.png";
+import bg8 from "@/assets/images/match/8bg.png";
+import finalBg from "@/assets/images/match/final-bg.png";
+import grayBg from "@/assets/images/match/gray-bg.png";
+import roundIng from "@/assets/images/match/round-ing.png";
+import ing4 from "@/assets/images/match/4ing.png";
+import ing8 from "@/assets/images/match/8ing.png";
+import finalIng from "@/assets/images/match/final-ing.png";
+import end from "@/assets/images/match/end.png";
+import pending from "@/assets/images/match/pending.png";
+import roundName from "@/assets/images/match/round-name.png";
+import name4 from "@/assets/images/match/4name.png";
+import name8 from "@/assets/images/match/8name.png";
+import finalName from "@/assets/images/match/final-name.png";
 
+const getImage = (item) => {
+  switch (item.type) {
+    case 1:
+      return {
+        bg: item.status === 0 ? grayBg : roundBg,
+        statusImg:
+          item.status === 0 ? pending : item.status === 1 ? roundIng : end,
+        name: roundName,
+      };
+    case 2:
+      return {
+        bg: item.status === 0 ? grayBg : bg8,
+        statusImg: item.status === 0 ? pending : item.status === 1 ? ing8 : end,
+        name: name8,
+      };
+    case 3:
+      return {
+        bg: item.status === 0 ? grayBg : bg4,
+        statusImg: item.status === 0 ? pending : item.status === 1 ? ing4 : end,
+        name: name4,
+      };
+    case 4:
+      return {
+        bg: item.status === 0 ? grayBg : finalBg,
+        statusImg:
+          item.status === 0 ? pending : item.status === 1 ? finalIng : end,
+        name: finalName,
+      };
+    default:
+      return {
+        bg: roundBg,
+        statusImg:
+          item.status === 0 ? pending : item.status === 1 ? roundIng : end,
+        name: roundName,
+      };
+  }
+};
 </script>
 
 <template>
   <Detail>
-    <div class="against-container" v-loading="loading" :style="{
-      '--bg-level-8':requireImg('/level/3.png',true),
-    }">
-      <div class="match-list">
-        <div class="match-list-item" v-for="item in levelData" :key="item.id" @click="handleJump(item.type)">
-          <div class="match-list-item-status" :style="{ backgroundColor: statusColor(item.status) }">{{ statusMap[item.status] }}</div>
-          <div class="match-list-item-title">{{ item.name }}</div>
-          <div class="time">
-            <img :src="luckyStar" alt="">
-            <div class="label">活动时间</div>
-            <div class="match-time">{{ item.startTime }} - {{ item.endTime }}</div>
-          </div>
+    <div class="against-container" v-loading="loading">
+      <div
+        class="match-list-item"
+        :style="{
+          background: `url(${getImage(item).bg}) no-repeat`,
+          'background-size': '100% 100%',
+        }"
+        v-for="item in levelData"
+        :key="item.id"
+        @click="handleJump(item.type)"
+      >
+        <img :src="getImage(item).name" class="name-image" alt="" />
+        <div class="time">
+          活动时间：{{ item.startTime }}-{{ item.endTime }}
         </div>
+        <img
+          :src="getImage(item).statusImg"
+          class="status-image"
+          :class="item.status === 1 ? 'ing' : ''"
+          alt=""
+        />
       </div>
     </div>
   </Detail>
@@ -75,61 +141,42 @@ onMounted(() => {
 
 <style scoped lang="scss">
 @use "@/style" as *;
-
-.match-list {
-  width: 94%;
+.against-container {
+  width: 1018px;
   margin: 0 auto;
-}
-.match-list-item {
-  background-color: rgba(50, 50, 50, 0.66);
-  background-position: bottom;
-  background-repeat: repeat-x;
-  background-image: var(--bg-level-8);
-  border-radius: 12px;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  cursor: pointer;
-  .match-list-item-status {
-    width: 120px;
-    height: 28px;
-    line-height: 28px;
-    font-size: 14px;
-    display: flex;
-    justify-content: center;
-    border-bottom-left-radius: 12px;
-    border-bottom-right-radius: 12px;
-  }
-  .match-list-item-title {
-    font-family: "titleFont", "Microsoft YaHei", 'sans-serif';
-    font-size: 28px;
-    color: rgb(241, 231, 245);
-    margin: 18px 0;
-  }
-  .time {
-    display: flex;
-    font-size: 16px;
-    margin-bottom: 12px;
-    img {
-      width: 22px;
-      height: 22px;
-      transform: translateY(10%);
-      margin-right: 4px;
+  padding-top: 16px;
+  .match-list-item {
+    width: 100%;
+    height: 210px;
+    margin-top: 24px;
+    overflow: hidden;
+    padding-left: 45px;
+    position: relative;
+    cursor: pointer;
+    .name-image {
+      margin-top: 33px;
+      width: 129px;
+      height: 35px;
+    }
+    .time {
+      margin-top: 14px;
+      font-weight: 500;
+      font-size: 26px;
+      color: #ffffff;
+    }
+    .status-image {
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 118px;
+      height: 124px;
+      &.ing {
+        width: 204px;
+        height: 203px;
+        right: 70px;
+        bottom: 0;
+      }
     }
   }
-  .label {
-    font-family: "titleFont", "Microsoft YaHei", 'sans-serif';
-    margin-right: 12px;
-  }
-  .match-time, .match-signup-time {
-    font-family: "titleFont", "Microsoft YaHei", 'sans-serif';
-    text-shadow:  0px 0px 4px rgb(247, 219, 77);
-  }
-}
-
-/* 响应式调整 */
-@media (max-width: 768px) {
 }
 </style>
